e321be3194435b47e41071aedc7415397a5e4949
junomak <junomak@hanluninfo.com>
2016-09-22 15:09:17 +0800

    static files management

--- pokemonGO/settings.py
+++ pokemonGO/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_URL = '/static/'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
